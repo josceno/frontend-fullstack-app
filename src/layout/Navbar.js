210f9d0b3962897ad0055f4746f288db308fbe34
josceno <jdamasceno414@gmail.com>
2023-02-19 17:13:56 -0300

    insert

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,9 +1,15 @@
+import axios from 'axios';
 import { Button } from 'bootstrap'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+function handleSubmit(event) {
+    event.preventDefault();
+  }
+ 
+
 export default function Navbar() {
-    const [cliente,setCliente]=useState({
+    const [cliente, setCliente] = useState({
         nome: "",
         cpf: "",
         endereco: "",
@@ -15,16 +21,19 @@ export default function Navbar() {
         observacao: "",
         rg: "",
         dataNascmemto: "",
-        cep: null
+        cep: ""
     })
 
-    const {nome,cpf,endereco,cidade,complemento,uf,
-        celular,telefone,observacao,rg,dataNascmemto,cep}=cliente
+    const { nome, cpf, endereco, cidade, complemento, uf,
+        celular, telefone, observacao, rg, dataNascmemto, cep } = cliente
 
-    const onInputChange=(e)=>{
+    const onInputChange = (e) => {
+        setCliente({ ...cliente, [e.target.name]: e.target.value })
+    }
 
-    }    
-
+    const onSubmit= async (e)=>{
+        await axios.post("http://localhost:8080/cliente",cliente)
+    };
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -39,7 +48,7 @@ export default function Navbar() {
                         aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <button data-bs-toggle="modal" data-bs-target="#janelaModal" className='btn btn-outline-light' to="/novoCliente" >Cadastrar Usuário</button>
+                    <button  data-bs-toggle="modal" data-bs-target="#janelaModal" className='btn btn-outline-light' to="/novoCliente" >Cadastrar Usuário</button>
                 </div>
             </nav>
 
@@ -48,63 +57,64 @@ export default function Navbar() {
                     <div class="modal-content">
                         <div class="modal-header">
                             <h3 class="modal-title">Novo Cliente</h3>
-                            <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>
+                            <button type="button" class="btn btn-close" ></button>
                         </div>
                         <div className='container'>
                             <div className='row'>
                                 <div className='col-md-6 offset-md-3 border rounded p-4 mt-2'>
                                     <div className='mb-3'>
-                                        <form>
-                                            <label htmlfor="nome">Nome:</label>
-                                            <input type="text" id="nome" name="nome" value={nome} onChange={(e)=>onInputChange(e)}/><br />
+                                        <form onSubmit={(e)=>onSubmit(e)} >
+                                            <label htmlFor="nome">Nome:</label>
+                                            <input type={"text"} className="form-control" id="nome" name="nome" value={nome} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="cpf">CPF:</label>
-                                            <input type="text" id="cpf" name="cpf" value={cpf} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="cpf">CPF:</label>
+                                            <input type="text" id="cpf" name="cpf" value={cpf} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="endereco">Endereço:</label>
-                                            <input type="text" id="endereco" name="endereco" value={endereco} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="endereco">Endereço:</label>
+                                            <input type="text" id="endereco" name="endereco" value={endereco} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="cidade">Cidade:</label>
-                                            <input type="text" id="cidade" name="cidade" value={cidade} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="cidade">Cidade:</label>
+                                            <input type="text" id="cidade" name="cidade" value={cidade} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="complemento">Complemento:</label>
-                                            <input type="text" id="complemento" name="complemento" value={complemento} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="complemento">Complemento:</label>
+                                            <input type="text" id="complemento" name="complemento" value={complemento} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="uf">UF:</label>
-                                            <input type="text" id="uf" name="uf" value={uf} onChange={(e)=>onInputChange(e)} /><br />
+                                            <label htmlFor="uf">UF:</label>
+                                            <input type="text" id="uf" name="uf" value={uf} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="celular">Celular:</label>
-                                            <input type="text" id="celular" name="celular" value={celular} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="celular">Celular:</label>
+                                            <input type="text" id="celular" name="celular" value={celular} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="telefone">Telefone:</label>
-                                            <input type="text" id="telefone" name="telefone" value={telefone} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="telefone">Telefone:</label>
+                                            <input type="text" id="telefone" name="telefone" value={telefone} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="observacao">Observação:</label>
-                                            <input type="text" id="observacao" name="observacao" value={observacao} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="observacao">Observação:</label>
+                                            <input type="text" id="observacao" name="observacao" value={observacao} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="rg">RG:</label>
-                                            <input type="text" id="rg" name="rg" value={rg} onChange={(e)=>onInputChange(e)}/><br />
+                                            <label htmlFor="rg">RG:</label>
+                                            <input type="text" id="rg" name="rg" value={rg} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="dataNascmemto">Data de Nascimento:</label>
-                                            <input type="date" id="dataNascmemto" name="dataNascmemto" value={dataNascmemto} onChange={(e)=>onInputChange(e)}/><br/>
+                                            <label htmlFor="dataNascmemto">Data de Nascimento:</label>
+                                            <input type="date" id="dataNascmemto" name="dataNascmemto" value={dataNascmemto} onChange={(e) => onInputChange(e)} /><br />
 
-                                            <label htmlfor="cep">CEP:</label>
-                                            <input type="text" id="cep" name="cep" value={cep} onChange={(e)=>onInputChange(e)}/><br />
-                                                
-                                            <button type='submmit' className='btn btn-outline-primary'>adcionar</button>
-                                            <button type='submmit' className='btn btn-outline-danger'>Cancelar</button>
-                                            </form>
+                                            <label htmlFor="cep">CEP:</label>
+                                            <input type="text" id="cep" name="cep" value={cep} onChange={(e) => onInputChange(e)} /><br />
 
-                                            </div>
+                                            <button  type='submit' className='btn btn-outline-primary'>adicionar</button>
+                                            
+                                            <button type='reset' className='btn btn-outline-danger'>Cancelar</button>
+                                        </form>
 
                                     </div>
+
                                 </div>
-
                             </div>
-                        </div>
 
+                        </div>
                     </div>
+
                 </div>
             </div>
-            )
+        </div>
+    )
 }
