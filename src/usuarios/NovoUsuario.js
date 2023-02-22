0622f394c11e31f77ccdb6be10036633cef412c3
josceno <jdamasceno414@gmail.com>
2023-02-22 09:08:11 -0300

    .

--- src/usuarios/NovoUsuario.js
+++ src/usuarios/NovoUsuario.js
@@ -1,63 +1,78 @@
 import axios from 'axios';
 import { Button } from 'bootstrap'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
-function handleSubmit(event) {
-    event.preventDefault();
-  }
- 
 
 export default function NovoUsuario() {
+    const [success, setSuccsses] = useState('');
+    let navigate = useNavigate()
     const [usuario, setusuario] = useState({
         nome: "",
         cpf: "",
         password: ""
     })
 
-    const { nome, cpf, password} = usuario
-
+    const { nome, cpf, password } = usuario
+        ;
     const onInputChange = (e) => {
         setusuario({ ...usuario, [e.target.name]: e.target.value })
     }
 
-    const onSubmit= async (e)=>{
-        await axios.post("http://localhost:8080/usuario",usuario)
+    const onSubmit = async (e) => {
+
+        await axios.post("http://localhost:8080/usuario", usuario)
+        setSuccsses(true)
     };
-  return (
-   
-     <section>   
-       <h1>Login</h1>
-       <form onSubmit={(e)=>onSubmit(e)}>
-            <label htmlFor='cpf'>Cpf:</label>
-            <input type={"text"} 
-             className="form-control"
-             name='cpf'
-            id="cpf" 
-            value={cpf} 
-            onChange={(e) => onInputChange(e)} 
-            required/>
-            <label htmlFor="nome">Nome:</label>
-            <input type={"text"} className="form-control" 
-            id="nome" 
-            name="nome" 
-            value={nome} onChange={(e) => onInputChange(e)}
-            required/>
+    return (
+        <>
+            {success ? (
+                navigate("/")
 
-            <label htmlFor='password'>Senha:</label>
-            <input type={"text"} 
-             className="form-control"
-             name='password'
-            id="password" 
-            value={password} 
-            onChange={(e) => onInputChange(e)} 
-            required/>
+            ) : (
+                <section>
 
-            <button>Entrar</button>
-       </form>
-    </section>
+                    <h1>Cadastro</h1>
+                    <form onSubmit={(e) => onSubmit(e)}  >
+                        <label htmlFor='cpf'>Cpf:</label>
+                        <input type={"text"}
+                            className="form-control"
+                            name='cpf'
+                            id="cpf"
+                            value={cpf}
+                            onChange={(e) => onInputChange(e)}
+                            required />
+                        <label htmlFor="nome">Nome:</label>
+                        <input type={"text"} className="form-control"
+                            id="nome"
+                            name="nome"
+                            value={nome} onChange={(e) => onInputChange(e)}
+                            required />
+
+                        <label htmlFor='password'>Senha:</label>
+                        <input type={"password"}
+                            className="form-control"
+                            name='password'
+                            id="password"
+                            value={password}
+                            onChange={(e) => onInputChange(e)}
+                            required />
+
+                        <button >Entrar</button>
+                    </form>
+                    <p>
+                        já tem usuário?<br />
+                        <span className="line">
+                            <Link to="/">Entrar</Link>
+                        </span>
+                    </p>
+                </section>
+
+            )}
+        </>
+
     )
-   
-  
+
+
 }
